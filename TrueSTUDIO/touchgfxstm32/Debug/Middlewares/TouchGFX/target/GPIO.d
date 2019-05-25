72007e3d382c19c1eefedb2bc2f1139e0ad5e1cd
jfrut <j.fruth2@gmail.com>
2019-05-25 14:01:54 +0200

    toch working

--- TrueSTUDIO/touchgfxstm32/Debug/Middlewares/TouchGFX/target/GPIO.d
+++
@@ -1,5 +0,0 @@
-Middlewares/TouchGFX/target/GPIO.o: \
- C:/Users/jfrut/development/mic_prog/touchgfxstm32/TouchGFX/target/GPIO.cpp \
- ../../../Middlewares/ST/TouchGFX/touchgfx/framework/include/touchgfx/hal/GPIO.hpp
-
-../../../Middlewares/ST/TouchGFX/touchgfx/framework/include/touchgfx/hal/GPIO.hpp:
